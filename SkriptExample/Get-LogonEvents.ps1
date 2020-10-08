0bb25536c40c195a0026db75c70268e68de3c8e7
Stefan Ober <StefanO@ppedv.de>
2020-10-08 13:44:21 +0200

    Skriptexample mit einfachen Parametern

---
+++ SkriptExample/Get-LogonEvents.ps1
@@ -0,0 +1,8 @@
+﻿[cmdletBinding()]
+Param(
+$Logname,
+$Computername,
+$EventId,
+$Newest
+)
+Get-EventLog -LogName $Logname -ComputerName $Computername | Where-Object -FilterScript {$_.EventID -eq $EventId} | Select-Object -First $Newest
