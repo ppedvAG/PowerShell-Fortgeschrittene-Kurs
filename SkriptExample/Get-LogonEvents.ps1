0f3487cc8143437ae0958d20016e59eb438ebe2e
Stefan Ober <StefanO@ppedv.de>
2020-10-08 13:57:41 +0200

    Skriptexample mit Default Values und Pflicht paramtern versehen

--- SkriptExample/Get-LogonEvents.ps1
+++ SkriptExample/Get-LogonEvents.ps1
@@ -1,8 +1,12 @@
-﻿[cmdletBinding()]
+﻿[cmdletBinding()] #Benutze Powershell default Parameter
 Param(
-$Logname,
-$Computername,
-$EventId,
-$Newest
+[string]$Logname = "Security",
+[string]$Computername = "localhost",
+[Parameter(Mandatory=$true)] #Parametereigenschaften wie zb Pflicht Parameter
+[int]$EventId,
+[int]$Newest = 5
 )
+
+#Verbose also zusätzliche Ausgabe die bei angabe von -Verbose mit ausgegeben wird
+Write-Verbose -Message "Das Skript wurde mit folgenden Werten gestartet. Logname: $Logname, EventID: $EventId"
 Get-EventLog -LogName $Logname -ComputerName $Computername | Where-Object -FilterScript {$_.EventID -eq $EventId} | Select-Object -First $Newest
